75d0c1cbfc93df59017b607d9ab8cb58c4b54fe1
wesowx <56712164+wesowx@users.noreply.github.com>
2020-03-03 14:00:16 +0800

    'd'

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const https = require('https');
 const bcrypt = require('bcryptjs');
+const knex = require('knex');
+const pg = require('pg');
 
 const app = express();
 
@@ -8,20 +10,26 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
-let db = [
-  {
-    id: 1,
-    username: "wesowx",
-    name: "wes",
-    password: "wes",
-    p: new Date(2020,1,16).getTime(),
-    m: new Date().getTime(),
-    o: new Date().getTime(),
-    fap: 1,
-    joined: new Date(),
-    rank: 'Fapper'
-  }
-]
+const db = knex({
+  client:'pg',
+  connection: process.env.DATABASE_URL,
+  searchPath: ['knex,public']
+})
+
+// let db = [
+//   {
+//     id: 1,
+//     username: "wesowx",
+//     name: "wes",
+//     password: "wes",
+//     p: new Date(2020,1,16).getTime(),
+//     m: new Date().getTime(),
+//     o: new Date().getTime(),
+//     fap: 1,
+//     joined: new Date(),
+//     rank: 'Fapper'
+//   }
+// ]
 
 app.get('/', (req,res) => console.log('hi'));
 
@@ -29,55 +37,101 @@ app.get('/', (req,res) => console.log('hi'));
 app.post(
   '/signin',
   (req,res) => {
-    let passwordValidation = bcrypt.compareSync(req.body.password, db[db.length-1].password);
-    if (req.body.username === db[db.length-1].username && passwordValidation) {
-      res.json(db[db.length-1]);
-    } else {
-      res.status(404).json('no such user');
-    }
+    const {username,password} = req.body;
+    db('users')
+    .where('username',username)
+    .then(user => {
+      const passwordValidation = bcrypt.compareSync(password, user[0].password);
+      if (passwordValidation) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json('no such user');
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+
+
+    // OLD CODE
+
+    // let passwordValidation = bcrypt.compareSync(req.body.password, db[db.length-1].password);
+    // if (req.body.username === db[db.length-1].username && passwordValidation) {
+    //   res.json(db[db.length-1]);
+    // } else {
+    //   res.status(404).json('no such user');
+    // }
   }
 );
 
 
 app.post('/register', (req,res) => {
-  if (!req.body.name.length || !req.body.username.length || !req.body.password.length) {
-    res.status(404).json("Incorrect credentials");
+  const {name,username,password} = req.body;
+
+  if (!name.length || !username.length || !password.length) {
+    res.status(404).json("Do not leave blanks")
   } else {
-    let hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
-    console.log(hashedPassword);
-
-    db.push({
-      id: db[db.length-1].id + 1,
-      username: req.body.username,
-      name: req.body.name,
+    let hashedPassword = bcrypt.hashSync(password,8);
+    db('users')
+    .returning(['*'])
+    .insert({
+      username: username,
+      name: name,
       password: hashedPassword,
       p: new Date().getTime(),
       m: new Date().getTime(),
       o: new Date().getTime(),
-      fap: 0,
-      joined: new Date(),
-      rank: 'Fapper'
-    });
-    res.json(db[db.length-1]);
+      joined: new Date()
+    })
+    .then(user => res.json[0])
+    .catch(err => res.status(404).json(err))
   }
+
+
+  // OLD CODE
+
+  // if (!req.body.name.length || !req.body.username.length || !req.body.password.length) {
+  //   res.status(404).json("Incorrect credentials");
+  // } else {
+  //   let hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  //
+  //   console.log(hashedPassword);
+  //
+  //   db.push({
+  //     id: db[db.length-1].id + 1,
+  //     username: req.body.username,
+  //     name: req.body.name,
+  //     password: hashedPassword,
+  //     p: new Date().getTime(),
+  //     m: new Date().getTime(),
+  //     o: new Date().getTime(),
+  //     fap: 0,
+  //     joined: new Date(),
+  //     rank: 'Fapper'
+  //   });
+  //   res.json(db[db.length-1]);
+  // }
 })
 
 
-app.post('/reset', (req,res) => {
-  if (req.body.p) {
-    db[0].p = new Date().getTime();
-  }
-  if (req.body.m) {
-    db[0].m = new Date().getTime();
-  }
-  if (req.body.o) {
-    db[0].o = new Date().getTime();
-    db[0].fap++
-  }
-
-  res.json(db[0]);
-})
+// app.post('/reset', (req,res) => {
+//   if (req.body.p) {
+//     db[length-1].p = new Date().getTime();
+//   }
+//   if (req.body.m) {
+//     db[length-1].m = new Date().getTime();
+//   }
+//   if (req.body.o) {
+//     db[length-1].o = new Date().getTime();
+//     db[length-1].fap++
+//   }
+//
+//   res.json(db[length-1]);
+// })
+//
+//
+// app.put('/updaterank', (req,res) => {
+//   db[length-1].rank = req.body.rank;
+//   res.json(db[length-1]);
+// })
 
 
 app.listen(process.env.PORT || 8080, () => {
